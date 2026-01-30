c0dbe5c815fecfb54d8d7d61d0a85b125e67091f
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-01-30 11:43:17 +0545

    docs: project preview gif

--- src/components/places.tsx
+++ src/components/places.tsx
@@ -4,7 +4,12 @@ import PlaceCard from "./place-card";
 export default function Places() {
   return (
     <section className="places">
-      <h1>Places</h1>
+      <div className="places-text">
+        <h1>Explore Places</h1>
+        <p>
+          Explore scenic view of different places from all around the world.
+        </p>
+      </div>
 
       <div className="places-grid">
         {placeData.map((place) => (
